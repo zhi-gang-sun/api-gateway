afb322495918759ae2c167d1eb82d290f7a7736d
owen <zgsun@cn.ibm.com>
2021-04-09 15:54:57 +0800

    initial commit for api-gateway, it talks to Eureka server to proxy incoming calls.

---
+++ src/test/java/com/ibm/cio/dsw/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ibm.cio.dsw.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
