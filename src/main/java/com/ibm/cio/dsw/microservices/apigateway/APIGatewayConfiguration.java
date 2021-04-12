88edeaa8373b44d8193a686d696002e4295d57c8
owen <zgsun@cn.ibm.com>
2021-04-12 14:49:00 +0800

    add logging filter, path rewrite etc API gateway frequently used functions.

---
+++ src/main/java/com/ibm/cio/dsw/microservices/apigateway/APIGatewayConfiguration.java
@@ -0,0 +1,23 @@
+package com.ibm.cio.dsw.microservices.apigateway;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class APIGatewayConfiguration {
+
+	@Bean
+	public RouteLocator gatewayRouter(RouteLocatorBuilder builder) {
+
+		// add request header and redirect to other URIs
+		return builder.routes()
+				.route(p -> p.path("/get").filters(
+						f -> f.addRequestHeader("MyHeader", "MyURI").addRequestParameter("MyParm", "MyParmValue"))
+						.uri("http://httpbin.org:80"))
+				.route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+				.route(p -> p.path("/currency-conversion/**").uri("lb://CURRENCY-CONVERSION-SERVICE"))
+				.build();
+	}
+}
